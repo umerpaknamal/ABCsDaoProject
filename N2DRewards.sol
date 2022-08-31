96a10800766725aad0952182466075c52602bb04
umerpaknamal <99263424+umerpaknamal@users.noreply.github.com>
2022-08-31 16:58:00 +0000

    Update N2DRewards.sol

--- N2DRewards.sol
+++ N2DRewards.sol
@@ -2,6 +2,13 @@
  *Submitted for verification at Etherscan.io on 2022-08-31
 */
 
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 pragma solidity 0.5.16;
 
 interface IBEP20 {
