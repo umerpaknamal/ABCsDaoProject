795cea31ce4495b98a34056aa98d029028f5daa5
umerpaknamal <99263424+umerpaknamal@users.noreply.github.com>
2022-09-20 20:30:53 +0500

    Update rewardtoken.sol

--- rewardtoken.sol
+++ rewardtoken.sol
@@ -1,6 +1,6 @@
 
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.7;
+pragma solidity 0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
