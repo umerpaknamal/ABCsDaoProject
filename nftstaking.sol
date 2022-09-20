b0e81fa51e7cf0829a59e0a696066f28ca0d6b33
umerpaknamal <99263424+umerpaknamal@users.noreply.github.com>
2022-09-20 20:27:10 +0500

    Update nftstaking.sol

--- nftstaking.sol
+++ nftstaking.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT LICENSE
 
-pragma solidity 0.8.7;
+pragma solidity 0.8.4;
 //import "./ABCs.sol";
 import "https://github.com/umerpaknamal/DaoProject/rewardtoken.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
